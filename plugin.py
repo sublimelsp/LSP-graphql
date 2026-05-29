9d58b78e64dab2aa8b5755154345c94061b3479e
Rafał Chłodnicki <rchl2k@gmail.com>
2026-05-29 21:32:38 +0200

    refactor: migrate to LspPlugin (#96)

--- plugin.py
+++ plugin.py
@@ -1,18 +1,31 @@
 from __future__ import annotations
 
-from lsp_utils import NpmClientHandler
-import os
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from lsp_utils import NodeManager
+from pathlib import Path
+from sublime_lib import ResourcePath
+from typing_extensions import override
 
 
 def plugin_loaded() -> None:
-    LspGraphqlPlugin.setup()
+    LspGraphqlPlugin.register()
 
 
 def plugin_unloaded() -> None:
-    LspGraphqlPlugin.cleanup()
+    LspGraphqlPlugin.unregister()
 
 
-class LspGraphqlPlugin(NpmClientHandler):
-    package_name = str(__package__)
-    server_directory = 'language-server'
-    server_binary_path = os.path.join(server_directory, 'start-server.js')
+class LspGraphqlPlugin(LspPlugin):
+
+    @classmethod
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath('Packages', package_name, 'language-server'),
+            Path('start-server.js'),
+            node_version_requirement='>=14',
+        )
